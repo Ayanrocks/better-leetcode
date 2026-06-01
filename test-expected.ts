d20f6453b20a35bfe07defc1514acff4778de022
Ayanrocks <ayanb1999@gmail.com>
2026-06-01 17:17:47 +0530

    removed unnecessary files

--- test-expected.ts
+++
@@ -1,3 +0,0 @@
-import { readFileSync } from 'fs';
-// I don't have the raw json from leetcode.
-// I will write a mock api call to check it!
--- test_script.ts
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-async function run() {
-    // Just a dummy to check if I can hit LeetCode to see response structure
-}
-run();
